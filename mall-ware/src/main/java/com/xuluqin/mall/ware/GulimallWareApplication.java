3783a1a8722d5391661c188b0ce44dcebabcdf44
Xu, Luqin <13262816312@163.com>
2024-03-31 01:30:10 +0800

    [P14] initial the project

---
+++ mall-ware/src/main/java/com/xuluqin/mall/ware/GulimallWareApplication.java
@@ -0,0 +1,16 @@
+package com.xuluqin.mall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//@EnableFeignClients
+//@EnableDiscoveryClient
+@SpringBootApplication
+public class GulimallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallWareApplication.class, args);
+    }
+
+}
