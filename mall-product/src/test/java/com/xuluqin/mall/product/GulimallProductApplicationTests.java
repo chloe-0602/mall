c5ded99dd076d1f24b750cb60d9df8a6784c8369
Xu, Luqin <13262816312@163.com>
2024-04-01 13:14:21 +0800

    [P18] feat: test mall-product insert update search successfully

--- mall-product/src/test/java/com/xuluqin/mall/product/GulimallProductApplicationTests.java
+++ mall-product/src/test/java/com/xuluqin/mall/product/GulimallProductApplicationTests.java
@@ -44,21 +44,20 @@ public class GulimallProductApplicationTests {
     public void contextLoads() {
 
         BrandEntity brandEntity = new BrandEntity();
-//        brandEntity.setBrandId(1L);
-//        brandEntity.setDescript("华为");
+        brandEntity.setBrandId(1L);
+        brandEntity.setDescript("华为");
 
 //
-        brandEntity.setName("华为");
-        brandService.save(brandEntity);
-        System.out.println("保存成功....");
+//        brandEntity.setName("华为");
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功....");
 
 //        brandService.updateById(brandEntity);
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+        list.forEach(item -> {
+            System.out.println(item);
+        });
 
-
-//        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
-//        list.forEach((item) -> {
-//            System.out.println(item);
-//        });
 
     }
 
