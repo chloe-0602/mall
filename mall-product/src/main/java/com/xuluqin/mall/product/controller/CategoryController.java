3f156d7ba86c81484049227a7f24bab4873e0477
Xu, Luqin <13262816312@163.com>
2024-04-11 07:46:32 +0800

    [P62] feat: test OSS

--- mall-product/src/main/java/com/xuluqin/mall/product/controller/CategoryController.java
+++ mall-product/src/main/java/com/xuluqin/mall/product/controller/CategoryController.java
@@ -51,7 +51,7 @@ public class CategoryController {
     public R info(@PathVariable("catId") Long catId){
 		CategoryEntity category = categoryService.getById(catId);
 
-        return R.ok().put("category", category);
+        return R.ok().put("data", category);
     }
 
     /**
@@ -76,6 +76,13 @@ public class CategoryController {
         return R.ok();
     }
 
+    @RequestMapping("/update/sort")
+    //@RequiresPermissions("product:category:update")
+    public R updateSort(@RequestBody CategoryEntity[] category){
+        categoryService.updateBatchById(Arrays.asList(category));
+
+        return R.ok();
+    }
     /**
      * 删除
      * @RequestBody:获取请求体，必须发送post请求
